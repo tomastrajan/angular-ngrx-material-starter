e5746e6ed4aab3eb6903000292d81ce382f67880
Angular CLI <angular-cli@angular.io>
2017-05-10 21:38:38 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularNgrxMaterialStarterPage } from './app.po';
+
+describe('angular-ngrx-material-starter App', () => {
+  let page: AngularNgrxMaterialStarterPage;
+
+  beforeEach(() => {
+    page = new AngularNgrxMaterialStarterPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
