fda508982a49df8e06e9c60ff0c76b91497a5d3a
tomastrajan <tomas.trajan@gmail.com>
2017-05-28 16:19:33 +0200

    feat: intro page

--- src/app/static/about/about.component.ts
+++ src/app/static/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  intro = require('../../../assets/intro.jpg');
+
   constructor() { }
 
   ngOnInit() {
