c49f75da8ee9ec0ff4d7f0329d0bec2437004d84
tomastrajan <tomas.trajan@gmail.com>
2017-07-24 22:06:01 +0200

    fix: remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 
 import { SharedModule } from '@app/shared';
 import { CoreModule } from '@app/core';
