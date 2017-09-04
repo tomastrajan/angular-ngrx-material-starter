4e56415545cd0abeb01e33762ff230b2d1ff4ded
Tomas Trajan <tomas.trajan@gmail.com>
2017-09-04 14:11:14 +0930

    fix: travis ci chrome env

--- karma.conf.js
+++ karma.conf.js
@@ -41,6 +41,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeTravisCi: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
