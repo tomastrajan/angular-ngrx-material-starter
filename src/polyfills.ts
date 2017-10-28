801757ad6ca4b9ebccc6cd4cf6dfba582dc793d9
tomastrajan <tomas.trajan@gmail.com>
2017-10-29 00:29:06 +0800

    feat(perf): explicitly import angular material components to achieve smaller bundle size, remove unnecessary polyfills, bump rxjs version

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -34,10 +34,7 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js'; // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// import 'classlist.js'; // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
