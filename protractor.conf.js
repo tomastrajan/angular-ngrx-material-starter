9026ff61c279116d20ffe81b0ab6dfd8ff70f5e5
tomastrajan <tomas.trajan@gmail.com>
2018-02-01 18:23:26 +1300

    fix(build): run e2e tests in CI environment

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,7 +9,10 @@ exports.config = {
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: ['--no-sandbox']
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
