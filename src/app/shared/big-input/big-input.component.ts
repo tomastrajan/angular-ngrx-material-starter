8e8fc492fa65a5bcc0b7e038b408b97f51b48aec
HH <hhubik@vis.org>
2018-04-11 13:59:16 -0400

    feat(app): Changed change detection strategy to OnPush in two components
    To improve performance in two "dumb" components:
    BigInputComponent and BigInputActionComponent
    
    closes tomastrajan/angular-ngrx-material-starter/#126

--- src/app/shared/big-input/big-input.component.ts
+++ src/app/shared/big-input/big-input.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'anms-big-input',
   templateUrl: './big-input.component.html',
-  styleUrls: ['./big-input.component.scss']
+  styleUrls: ['./big-input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BigInputComponent {
   @Input() placeholder: string;
