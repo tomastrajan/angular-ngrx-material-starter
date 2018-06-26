27cc7976cde026edaab682b76fb5c06d8dafd964
Tomas Trajan <tomas.trajan@gmail.com>
2018-06-26 12:22:27 +0800

    fix(polyfills): add missing hammerjs, fixes #247

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -74,3 +74,5 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'hammerjs';
