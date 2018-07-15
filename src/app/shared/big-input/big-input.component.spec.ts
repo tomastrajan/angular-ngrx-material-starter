0211964b29ce0c994cc11f47d11109af8b345267
HH <hhubik@vis.org>
2018-07-15 11:46:59 -0400

    test(BigInputComponent): created unit test for BigInputComponent
    fixes #271

--- src/app/shared/big-input/big-input.component.spec.ts
+++ src/app/shared/big-input/big-input.component.spec.ts
@@ -1,28 +1,93 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SharedModule } from '@app/shared';
 
-import { BigInputComponent } from './big-input.component';
+@Component({
+  selector: 'host-for-test',
+  template: `
+  <anms-big-input placeholder="I am going to do..."
+  [value]="newValue"
+  (keyup)="onKeyEvent($event)"
+  (keyup.enter)="onKeyEvent($event)"
+  (keyup.escape)="onKeyEvent($event)">
+</anms-big-input>
+`
+})
+class HostComponent {
+  newValue: string;
+  onKeyEvent(eventData: any) {}
+}
 
 describe('BigInputComponent', () => {
-  let component: BigInputComponent;
-  let fixture: ComponentFixture<BigInputComponent>;
-
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        imports: [SharedModule]
-      }).compileComponents();
-    })
-  );
+  let fixture: ComponentFixture<HostComponent>;
+  let component: HostComponent;
+  let bigInputDebugElement: DebugElement;
+  let inputNativeElement: HTMLInputElement;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BigInputComponent);
+    TestBed.configureTestingModule({
+      declarations: [HostComponent],
+      imports: [SharedModule]
+    });
+    fixture = TestBed.createComponent(HostComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    spyOn(component, 'onKeyEvent');
+    bigInputDebugElement = fixture.debugElement.childNodes[0] as DebugElement;
+    inputNativeElement = fixture.nativeElement.querySelector('input');
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should have expected placeholder', () => {
+    expect(bigInputDebugElement.attributes['placeholder']).toContain(
+      'going to do'
+    );
+  });
+
+  it('should have updatable value', () => {
+    component.newValue = 'abcde';
+    fixture.detectChanges();
+    expect(inputNativeElement.value).toBe(component.newValue);
+  });
+
+  it('should respond to keyup event', () => {
+    const expectedEventData = 'Key UP';
+    bigInputDebugElement.triggerEventHandler('keyup', expectedEventData);
+
+    expect(component.onKeyEvent).toHaveBeenCalledTimes(1);
+    expect(component.onKeyEvent).toHaveBeenCalledWith(expectedEventData);
+  });
+
+  it('should respond to keyup.enter event', () => {
+    const expectedEventData = 'Enter UP';
+    bigInputDebugElement.triggerEventHandler('keyup.enter', expectedEventData);
+
+    expect(component.onKeyEvent).toHaveBeenCalledTimes(1);
+    expect(component.onKeyEvent).toHaveBeenCalledWith(expectedEventData);
+  });
+
+  it('should respond to escape event', () => {
+    const expectedEventData = 'Escape UP';
+    bigInputDebugElement.triggerEventHandler('keyup.escape', expectedEventData);
+
+    expect(component.onKeyEvent).toHaveBeenCalledTimes(1);
+    expect(component.onKeyEvent).toHaveBeenCalledWith(expectedEventData);
+  });
+
+  it('should respond to focus and blur', () => {
+    expect(bigInputDebugElement.componentInstance.hasFocus).toBe(
+      false,
+      'before focus'
+    );
+
+    inputNativeElement.dispatchEvent(new Event('focus'));
+    expect(bigInputDebugElement.componentInstance.hasFocus).toBe(
+      true,
+      'after focus'
+    );
+
+    inputNativeElement.dispatchEvent(new Event('blur'));
+    expect(bigInputDebugElement.componentInstance.hasFocus).toBe(
+      false,
+      'after blur'
+    );
   });
 });
