1bc4a1786f08aa7d81a9844c5065004e0f81bf7e
HH <hhubik@vis.org>
2018-08-09 01:23:29 -0400

    test(settings): added SettingsEffects unit test
    closes #270

---
+++ src/app/settings/settings.effects.spec.ts
@@ -0,0 +1,90 @@
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
+import { EffectsMetadata, getEffectsMetadata } from '@ngrx/effects';
+import { Observable } from 'rxjs';
+import { cold } from 'jasmine-marbles';
+
+import { SettingsEffects } from './settings.effects';
+import { AnimationsService, LocalStorageService } from '@app/core';
+import {
+  SettingsActionTypes,
+  SETTINGS_KEY
+} from '@app/settings/settings.reducer';
+import {
+  ActionSettingsPersist,
+  SettingsState,
+  ActionSettingsChangeLanguage
+} from '@app/settings';
+
+describe('SettingsEffects', () => {
+  let actions$: Observable<Action>;
+  let effects: SettingsEffects;
+  let metadata: EffectsMetadata<SettingsEffects>;
+  let localStorageService: any;
+  let animationsService: any;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SettingsEffects,
+        provideMockActions(() => actions$),
+        {
+          provide: LocalStorageService,
+          useValue: jasmine.createSpyObj('LocalStorageService', ['setItem'])
+        },
+        {
+          provide: AnimationsService,
+          useValue: jasmine.createSpyObj('AnimationsService', [
+            'updateRouteAnimationType'
+          ])
+        }
+      ]
+    });
+
+    localStorageService = TestBed.get(LocalStorageService);
+    animationsService = TestBed.get(AnimationsService);
+    effects = TestBed.get(SettingsEffects);
+  });
+
+  it('should not dispatch any action', () => {
+    metadata = getEffectsMetadata(effects);
+    expect(metadata.persistSettings).toEqual({ dispatch: false });
+  });
+
+  it('should call methods on AnimationsService and LocalStorageService for PERSIST action', () => {
+    const settings: SettingsState = {
+      language: 'en',
+      pageAnimations: true,
+      elementsAnimations: true,
+      theme: 'default',
+      autoNightMode: false,
+      pageAnimationsDisabled: true
+    };
+
+    const persistAction = new ActionSettingsPersist({ settings: settings });
+    actions$ = cold('a', { a: persistAction });
+    effects.persistSettings().subscribe(() => {
+      expect(localStorageService.setItem).toHaveBeenCalledWith(
+        SETTINGS_KEY,
+        persistAction.payload.settings
+      );
+      expect(animationsService.updateRouteAnimationType).toHaveBeenCalledWith(
+        true,
+        true
+      );
+    });
+  });
+
+  it('should not call methods on AnimationsService and LocalStorageService for other actions', () => {
+    const nonPersistAction: ActionSettingsChangeLanguage = new ActionSettingsChangeLanguage(
+      { language: 'en' }
+    );
+
+    actions$ = cold('a-|', { a: nonPersistAction });
+    effects.persistSettings().subscribe(undefined, undefined, () => {
+      expect(localStorageService.setItem).not.toHaveBeenCalled();
+      expect(animationsService.updateRouteAnimationType).not.toHaveBeenCalled();
+    });
+  });
+});
