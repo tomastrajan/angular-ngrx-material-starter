8144adc0cc2f1dd93a15bfe30184c755b8a76e5b
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-10-02 08:10:15 +0200

    test(settings): add unit tests for settings actions (#366)
    Closes #338

---
+++ src/app/settings/settings.actions.spec.ts
@@ -0,0 +1,108 @@
+import {
+  ActionSettingsChangeAnimationsElements,
+  ActionSettingsChangeAnimationsPage,
+  ActionSettingsChangeAnimationsPageDisabled,
+  ActionSettingsChangeAutoNightMode,
+  ActionSettingsChangeLanguage,
+  ActionSettingsChangeStickyHeader,
+  ActionSettingsChangeTheme,
+  ActionSettingsPersist,
+  SettingsActionTypes
+} from './settings.actions';
+import { NIGHT_MODE_THEME } from './settings.model';
+
+describe('Settings Actions', () => {
+  it('should create ActionSettingsChangeTheme action', () => {
+    const action = new ActionSettingsChangeTheme({
+      theme: NIGHT_MODE_THEME
+    });
+
+    expect(action.type).toEqual(SettingsActionTypes.CHANGE_THEME);
+    expect(action.payload.theme).toEqual(NIGHT_MODE_THEME);
+  });
+
+  it('should create ActionSettingsChangeAnimationsElements action', () => {
+    const action = new ActionSettingsChangeAnimationsElements({
+      elementsAnimations: true
+    });
+
+    expect(action.type).toEqual(SettingsActionTypes.CHANGE_ANIMATIONS_ELEMENTS);
+    expect(action.payload.elementsAnimations).toEqual(true);
+  });
+
+  it('should create ActionSettingsChangeAnimationsPage action', () => {
+    const action = new ActionSettingsChangeAnimationsPage({
+      pageAnimations: true
+    });
+
+    expect(action.type).toEqual(SettingsActionTypes.CHANGE_ANIMATIONS_PAGE);
+    expect(action.payload.pageAnimations).toEqual(true);
+  });
+
+  it('should create ActionSettingsChangeAnimationsPageDisabled action', () => {
+    const action = new ActionSettingsChangeAnimationsPageDisabled({
+      pageAnimationsDisabled: true
+    });
+
+    expect(action.type).toEqual(
+      SettingsActionTypes.CHANGE_ANIMATIONS_PAGE_DISABLED
+    );
+    expect(action.payload.pageAnimationsDisabled).toEqual(true);
+  });
+
+  it('should create ActionSettingsChangeAutoNightMode action', () => {
+    const action = new ActionSettingsChangeAutoNightMode({
+      autoNightMode: true
+    });
+
+    expect(action.type).toEqual(
+      SettingsActionTypes.CHANGE_AUTO_NIGHT_AUTO_MODE
+    );
+    expect(action.payload.autoNightMode).toEqual(true);
+  });
+
+  it('should create ActionSettingsChangeLanguage action', () => {
+    const action = new ActionSettingsChangeLanguage({
+      language: 'en'
+    });
+
+    expect(action.type).toEqual(SettingsActionTypes.CHANGE_LANGUAGE);
+    expect(action.payload.language).toEqual('en');
+  });
+
+  it('should create ActionSettingsChangeStickyHeader action', () => {
+    const action = new ActionSettingsChangeStickyHeader({
+      stickyHeader: true
+    });
+
+    expect(action.type).toEqual(SettingsActionTypes.CHANGE_STICKY_HEADER);
+    expect(action.payload.stickyHeader).toEqual(true);
+  });
+
+  it('should create ActionSettingsPersist action', () => {
+    const action = new ActionSettingsPersist({
+      settings: {
+        autoNightMode: true,
+        elementsAnimations: true,
+        language: 'en',
+        theme: NIGHT_MODE_THEME,
+        pageAnimations: true,
+        pageAnimationsDisabled: true,
+        stickyHeader: true
+      }
+    });
+
+    expect(action.type).toEqual(SettingsActionTypes.PERSIST);
+    expect(action.payload.settings).toEqual(
+      jasmine.objectContaining({
+        autoNightMode: true,
+        elementsAnimations: true,
+        language: 'en',
+        theme: NIGHT_MODE_THEME,
+        pageAnimations: true,
+        pageAnimationsDisabled: true,
+        stickyHeader: true
+      })
+    );
+  });
+});
