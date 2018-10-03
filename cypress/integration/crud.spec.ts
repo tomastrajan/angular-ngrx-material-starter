22e46f3b525358ec7b0a7449dcf6371f7fbee6af
Vishal Sodani <vishalsodani@rediffmail.com>
2018-10-03 22:02:46 +0530

    refactor(CRUD): add attributes to various elements in crud demo and use these attributes in cypress tests (#373)

--- cypress/integration/crud.spec.ts
+++ cypress/integration/crud.spec.ts
@@ -1,50 +1,50 @@
 const book_title = 'The Little Schemer';
 
 describe('CRUD Actions', () => {
-	context('Valid Data', function(){
-		beforeEach(() => {
-			cy.visit('/#/examples/crud');
-			cy.get('.add').click();
-			cy.get('#mat-input-0').type(book_title);
-			cy.get('#mat-input-1').type('Friedman{enter}');
-		});
-	  
-		it('should display a book added', () => {
-			cy.get('mat-card>h3').should('contain', book_title);
-		});
-	  
-		it('should not display a book deleted', () => {
-			cy.get('mat-card').first().click();
-			cy.get('[ng-reflect-message="Delete book"]').click();
-			cy.get('mat-card>h3').should(titles => {
-				expect(titles).to.have.length(1);
-			});
-		
-		});
-	  
-		it('should update a book title', () => {
-			cy.get('mat-card').first().click();
-			cy.get('[ng-reflect-message="Edit book"]').click();
-			cy.get('#mat-input-0').type(', 4th Edition{enter}');
-			cy.get('h3').should('contain', '4th Edition');
-		
-		});
+  context('Valid Data', function() {
+    beforeEach(() => {
+      cy.visit('/#/examples/crud');
+      cy.get('[data-testid="add-crud"]').click();
+      cy.get('[placeholder="Title"]').type(book_title);
+      cy.get('[placeholder="Author"]').type('Friedman{enter}');
+    });
 
-	});
-	
-	context('Empty book title and author', function(){
-		beforeEach(() => {
-			cy.visit('/#/examples/crud');
-			cy.get('.add').click();
-		});
-	  
-		it('should display errors', () => {
-			cy.get('#mat-input-1').type('{enter}');
-			cy.get('mat-error').should(errors => {
-				expect(errors).to.have.length(2);
-			});
-		});
-		
-	});
+    it('should display a book added', () => {
+      cy.get('[data-testid="item-title"]').should('contain', book_title);
+    });
 
+    it('should not display a book deleted', () => {
+      cy
+        .get('[data-testid="crud-item"]')
+        .first()
+        .click();
+      cy.get('[data-testid="delete-crud"]').click();
+      cy.get('[data-testid="item-title"]').should(titles => {
+        expect(titles).to.have.length(1);
+      });
+    });
+
+    it('should update a book title', () => {
+      cy
+        .get('[data-testid="crud-item"]')
+        .first()
+        .click();
+      cy.get('[data-testid="edit-crud"]').click();
+      cy.get('[placeholder="Title"]').type(', 4th Edition{enter}');
+      cy.get('[data-testid="item-title"]').should('contain', '4th Edition');
+    });
+  });
+
+  context('Empty book title and author', function() {
+    beforeEach(() => {
+      cy.visit('/#/examples/crud');
+      cy.get('[data-testid="add-crud"]').click();
+    });
+
+    it('should display errors', () => {
+      cy.get('[placeholder="Title"]').type('{enter}');
+      cy.get('[data-testid="error-title-crud"]').should('be.visible');
+      cy.get('[data-testid="error-author-crud"]').should('be.visible');
+    });
+  });
 });
