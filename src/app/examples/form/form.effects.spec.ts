bb7be53e50c8e094008476b6b77201487e019754
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-10-14 12:59:41 +0200

    test(form): add form effects tests

---
+++ src/app/examples/form/form.effects.spec.ts
@@ -0,0 +1,50 @@
+import { LocalStorageService } from '@app/core';
+import { Actions, getEffectsMetadata } from '@ngrx/effects';
+import { cold } from 'jasmine-marbles';
+import { EMPTY } from 'rxjs';
+import { ActionFormUpdate } from './form.actions';
+import { FormEffects, FORM_KEY } from './form.effects';
+import { Form } from './form.model';
+
+describe('FormEffects', () => {
+  let localStorageService: LocalStorageService;
+
+  beforeEach(() => {
+    localStorageService = jasmine.createSpyObj('LocalStorageService', [
+      'setItem'
+    ]);
+  });
+
+  describe('persistForm', () => {
+    it('should not dispatch any action', () => {
+      const actions = new Actions(EMPTY);
+      const effect = new FormEffects(actions, localStorageService);
+      const metadata = getEffectsMetadata(effect);
+
+      expect(metadata.persistForm).toEqual({ dispatch: false });
+    });
+
+    it('should call setItem on LocalStorageService for UPDATE action', () => {
+      const form: Form = {
+        autosave: false,
+        username: 'test',
+        password: 'test',
+        email: 'test@test.test',
+        description: 'It is a test.',
+        requestGift: true,
+        birthday: new Date(),
+        rating: 10
+      };
+      const action = new ActionFormUpdate({ form });
+      const source = cold('a', { a: action });
+      const actions = new Actions(source);
+      const effect = new FormEffects(actions, localStorageService);
+
+      effect.persistForm.subscribe(() => {
+        expect(localStorageService.setItem).toHaveBeenCalledWith(FORM_KEY, {
+          form
+        });
+      });
+    });
+  });
+});
