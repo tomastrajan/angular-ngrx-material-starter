cd761f4967445462608816ba7a0f577bca80a73c
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2018-11-01 13:22:03 +0100

    test(Form): submit form (#411)

--- src/app/examples/form/components/form.component.spec.ts
+++ src/app/examples/form/components/form.component.spec.ts
@@ -1,35 +1,104 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Store } from '@ngrx/store';
+import { createComponent, RenderResult } from 'ngx-testing-library';
 
 import { MockStore, TestingModule } from '@testing/utils';
 
+import { NotificationService } from '@app/core/notifications/notification.service';
 import { State } from '../../examples.state';
 import { FormState } from '../form.model';
-import { initialState } from '../form.reducer';
 import { FormComponent } from './form.component';
-import { NotificationService } from '@app/core/notifications/notification.service';
+import { initialState } from '../form.reducer';
+import { ActionFormUpdate } from '../form.actions';
 
-describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+fdescribe('FormComponent', () => {
   let store: MockStore<State>;
+  let component: RenderResult;
+  let dispatchSpy: jasmine.Spy;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [TestingModule],
-      declarations: [FormComponent],
-      providers: [NotificationService]
-    }).compileComponents();
+  beforeEach(async () => {
+    component = await createComponent<FormComponent>(
+      '<anms-form></anms-form>',
+      {
+        declarations: [FormComponent],
+        imports: [TestingModule],
+        providers: [NotificationService],
+        detectChanges: false
+      }
+    );
 
     store = TestBed.get(Store);
     store.setState(createState(initialState));
-    fixture = TestBed.createComponent(FormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    dispatchSpy = spyOn(store, 'dispatch');
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    component.fixture.detectChanges();
+  });
+
+  it('should submit when form is valid', async () => {
+    component.input(
+      component.getByLabelText('anms.examples.form.placeholder1'),
+      {
+        target: {
+          value: '@tim_deschryver'
+        }
+      }
+    );
+    component.input(
+      component.getByLabelText('anms.examples.form.placeholder2'),
+      {
+        target: {
+          value: 'mysuperawesomeandsecurepassword'
+        }
+      }
+    );
+    component.input(
+      component.getByLabelText('anms.examples.form.placeholder3'),
+      {
+        target: {
+          value: 'foo@bar.baz'
+        }
+      }
+    );
+    component.input(
+      component.getByLabelText('anms.examples.form.placeholder5'),
+      {
+        target: {
+          value: '1991-12-31'
+        }
+      }
+    );
+    component.input(
+      component.getByLabelText('anms.examples.form.placeholder4'),
+      {
+        target: {
+          value: 'no description needed here'
+        }
+      }
+    );
+    component.click(component.getByLabelText('anms.examples.form.text1'));
+    component.click(component.getByText('anms.examples.form.send'));
+    const { type, payload } = new ActionFormUpdate({
+      form: {
+        autosave: false,
+        username: '@tim_deschryver',
+        password: 'mysuperawesomeandsecurepassword',
+        email: 'foo@bar.baz',
+        description: 'no description needed here',
+        requestGift: true,
+        birthday: new Date(1991, 12, 31, 0, 0, 0),
+        rating: 0
+      }
+    });
+    const { birthday, ...formValue } = payload.form;
+    expect({ ...dispatchSpy.calls.mostRecent().args[0] }).toEqual({
+      type,
+      payload: {
+        form: jasmine.objectContaining({
+          ...formValue
+        })
+      }
+    });
+    expect(birthday).toBeTruthy();
   });
 });
 
