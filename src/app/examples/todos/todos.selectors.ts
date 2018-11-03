35630419dc79d1b12d02322a1634472847938a5d
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2018-11-03 13:19:01 +0100

    refactor: introduce todos selectors
    Rewrite tests using @angular-extensions/testing-library

--- src/app/examples/todos/todos.selectors.ts
+++ src/app/examples/todos/todos.selectors.ts
@@ -2,7 +2,35 @@ import { createSelector } from '@ngrx/store';
 
 import { ExamplesState, selectExamples } from '../examples.state';
 
-export const selectTodos = createSelector(
+export const selectTodosState = createSelector(
   selectExamples,
   (state: ExamplesState) => state.todos
+);
+
+export const selectTodosItems = createSelector(
+  selectTodosState,
+  state => state.items
+);
+
+export const selectTodosFilter = createSelector(
+  selectTodosState,
+  state => state.filter
+);
+
+export const selectTodos = createSelector(
+  selectTodosItems,
+  selectTodosFilter,
+  (items, filter) => {
+    if (filter === 'ALL') {
+      return items;
+    } else {
+      const predicate = filter === 'DONE' ? t => t.done : t => !t.done;
+      return items.filter(predicate);
+    }
+  }
+);
+
+export const removeDoneDisabled = createSelector(
+  selectTodosItems,
+  items => !items.some(item => item.done)
 );
