1bb6caeb8f5fa2de88b0c749c641bbf1b85002ea
tomastrajan <tomas.trajan@gmail.com>
2018-11-05 22:42:29 +0100

    refactor(auth): use existing selector instead of inline map

--- src/app/core/auth/auth-guard.service.ts
+++ src/app/core/auth/auth-guard.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
-import { selectAuth } from './auth.selectors';
+import { selectIsAuthenticated } from './auth.selectors';
 import { AppState } from '../core.state';
 
 @Injectable()
@@ -12,9 +11,6 @@ export class AuthGuardService implements CanActivate {
   constructor(private store: Store<AppState>) {}
 
   canActivate(): Observable<boolean> {
-    return this.store.pipe(
-      select(selectAuth),
-      map(auth => auth.isAuthenticated)
-    );
+    return this.store.pipe(select(selectIsAuthenticated));
   }
 }
