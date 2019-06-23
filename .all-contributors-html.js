c6a0db2ac41e730040a98daa851b6dc7fb62b778
tomastrajan <tomas.trajan@gmail.com>
2019-06-23 23:24:58 +0200

    fix(contributors): fix path in generate contributors script
    - add missing contributors

--- .all-contributors-html.js
+++ .all-contributors-html.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 const SOURCE = '.all-contributorsrc';
-const TARGET = './src/app/static/about/about.component.html';
+const TARGET =
+  './projects/angular-ngrx-material-starter/src/app/features/about/about/about.component.html';
 const TOKEN_START = '<!-- ALL-CONTRIBUTORS-LIST:START -->';
 const TOKEN_END = '<!-- ALL-CONTRIBUTORS-LIST:END -->';
 const PATTERN = new RegExp(`${TOKEN_START}[\\s\\S]*${TOKEN_END}`, 'gim');
@@ -9,12 +10,16 @@ const PATTERN = new RegExp(`${TOKEN_START}[\\s\\S]*${TOKEN_END}`, 'gim');
 const contributors = JSON.parse(fs.readFileSync(SOURCE, 'utf8')).contributors;
 const content = fs.readFileSync(TARGET, 'utf8');
 
-const data = contributors.map(c => `
+const data = contributors
+  .map(
+    c => `
   <a class="contributor" href="${c.profile}" target="_blank">
     <img src="${c.avatar_url}">
     <span>${c.name}</span>
   </a>
-`).join('\n');
+`
+  )
+  .join('\n');
 const replacement = `${TOKEN_START}${data}${TOKEN_END}`;
 const adjustedContent = content.replace(PATTERN, replacement);
 
