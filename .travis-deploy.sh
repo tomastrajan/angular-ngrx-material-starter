5464144c374678b59831efbdf1dd9af0bec510e6
tomastrajan <tomas.trajan@gmail.com>
2019-06-24 14:54:10 +0200

    fix(ci): adjust path in deployment script

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -13,7 +13,7 @@ else
 
   # go to the directory which contains build artifacts and create a *new* Git repo
   # directory may be different based on your particular build process
-  cd dist
+  cd dist/angular-ngrx-material-starter
   git init
 
   # inside this git repo we'll pretend to be a new user
