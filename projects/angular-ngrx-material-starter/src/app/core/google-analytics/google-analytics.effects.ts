ab81e5c44a247d9f874c1a3fc6ebe402d9f50482
Abed Zantout <abed@techhive.io>
2019-07-15 23:46:11 +0300

    refactor(google-analytics): refactor to ngrx 8

--- projects/angular-ngrx-material-starter/src/app/core/google-analytics/google-analytics.effects.ts
+++ projects/angular-ngrx-material-starter/src/app/core/google-analytics/google-analytics.effects.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { Effect } from '@ngrx/effects';
+import { createEffect } from '@ngrx/effects';
 import { tap, filter } from 'rxjs/operators';
 
 @Injectable()
 export class GoogleAnalyticsEffects {
   constructor(private router: Router) {}
 
-  @Effect({ dispatch: false })
-  pageView = this.router.events.pipe(
-    filter(event => event instanceof NavigationEnd),
-    tap((event: NavigationEnd) => {
-      (<any>window).ga('set', 'page', event.urlAfterRedirects);
-      (<any>window).ga('send', 'pageview');
-    })
+  pageView = createEffect(
+    () => () =>
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        tap((event: NavigationEnd) => {
+          (<any>window).ga('set', 'page', event.urlAfterRedirects);
+          (<any>window).ga('send', 'pageview');
+        })
+      ),
+    { dispatch: false }
   );
 }
